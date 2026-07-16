fd586bc9efc10bdcc5b7e756dede811f7b498927
brysondharper <brysondharper@gmail.com>
2026-07-16 11:48:24 -0600

    Update App.js

--- blacksmith-inventory/src/App.js
+++ blacksmith-inventory/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { initializeApp } from "firebase/app";
 import {
   getFirestore, collection, doc, onSnapshot,
